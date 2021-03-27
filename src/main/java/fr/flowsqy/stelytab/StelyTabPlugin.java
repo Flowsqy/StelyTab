302a4ab01ff17ab9df63b590ba521d779c838065
Flowsqy <Flo504.Dev@gmail.com>
2021-03-28 00:25:25 +0100

    Implement dependencies

--- src/main/java/fr/flowsqy/stelytab/StelyTabPlugin.java
+++ src/main/java/fr/flowsqy/stelytab/StelyTabPlugin.java
@@ -1,6 +1,37 @@
 package fr.flowsqy.stelytab;
 
+import fr.flowsqy.teampacketmanager.TeamPacketManager;
+import fr.flowsqy.teampacketmanager.TeamPacketManagerPlugin;
+import net.milkbowl.vault.permission.Permission;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class StelyTabPlugin extends JavaPlugin {
+
+    private Permission permission;
+    private TeamPacketManager teamPacketManager;
+
+    @Override
+    public void onEnable() {
+        final RegisteredServiceProvider<Permission> service = Bukkit.getServicesManager().getRegistration(Permission.class);
+        if(service == null) {
+            Bukkit.getConsoleSender().sendMessage("Pas de permission enregistré, plugin desactivé");
+            Bukkit.getPluginManager().disablePlugin(this);
+            return;
+        }
+
+        permission = service.getProvider();
+
+        final TeamPacketManagerPlugin teamPacketManagerPlugin = JavaPlugin.getPlugin(TeamPacketManagerPlugin.class);
+        teamPacketManager = teamPacketManagerPlugin.getTeamPacketManager();
+    }
+
+    public Permission getPermission() {
+        return permission;
+    }
+
+    public TeamPacketManager getTeamPacketManager() {
+        return teamPacketManager;
+    }
 }
