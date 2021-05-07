9363a76b259618c14257afec6debc20d951ee1e1
Flowsqy <Flo504.Dev@gmail.com>
2021-05-07 09:38:08 +0200

    Remove useless import

--- src/main/java/fr/flowsqy/stelytab/io/NameManager.java
+++ src/main/java/fr/flowsqy/stelytab/io/NameManager.java
@@ -15,7 +15,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
 
 import java.io.*;
 import java.sql.Timestamp;
